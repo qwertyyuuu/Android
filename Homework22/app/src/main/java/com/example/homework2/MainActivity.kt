3aad5b952fe6990bb4a71308094cd973755a76f0
qwertyyuuu <i9047691056@gmail.com>
2022-10-02 22:59:01 +0300

    homework2

---
+++ Homework22/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.homework2
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
